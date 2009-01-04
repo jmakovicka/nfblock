2860ecd31a6d28778b71b6df9637336e2d2bcdc9
Jindrich Makovicka <makovick@gmail.com>
2009-01-04 16:56:11 +0100

    untabify

--- debian/init.d
+++ debian/init.d
@@ -27,38 +27,38 @@ fi
 
 case "$1" in
     start)
-	echo -n "Starting $DESC:"
-	echo -n " nfblockd"
-	NFBLOCKD_ARGS="-d $BLOCKLIST_FILE -p $PIDFILE"
-	if [ "$DBUS" = "0" ]; then
-	    NFBLOCKD_ARGS="$NFBLOCKD_ARGS --no-dbus"
-	fi
-	if [ "$SYSLOG" = "0" ]; then
-	    NFBLOCKD_ARGS="$NFBLOCKD_ARGS --no-syslog"
-	fi
-	start-stop-daemon --start --quiet --oknodo --pidfile $PIDFILE --exec $DAEMON -- $NFBLOCKD_ARGS \
-	    < /dev/null
-	echo "."
-	;;
+        echo -n "Starting $DESC:"
+        echo -n " nfblockd"
+        NFBLOCKD_ARGS="-d $BLOCKLIST_FILE -p $PIDFILE"
+        if [ "$DBUS" = "0" ]; then
+            NFBLOCKD_ARGS="$NFBLOCKD_ARGS --no-dbus"
+        fi
+        if [ "$SYSLOG" = "0" ]; then
+            NFBLOCKD_ARGS="$NFBLOCKD_ARGS --no-syslog"
+        fi
+        start-stop-daemon --start --quiet --oknodo --pidfile $PIDFILE --exec $DAEMON -- $NFBLOCKD_ARGS \
+            < /dev/null
+        echo "."
+        ;;
     stop)
-	echo -n "Stopping $DESC:"
-	echo -n " nfblockd"
-	start-stop-daemon --stop --quiet --oknodo --pidfile $PIDFILE --exec $DAEMON
-	echo "."
-	;;
+        echo -n "Stopping $DESC:"
+        echo -n " nfblockd"
+        start-stop-daemon --stop --quiet --oknodo --pidfile $PIDFILE --exec $DAEMON
+        echo "."
+        ;;
     reload|force-reload)
-	echo -n "Reloading $DESC:"
-	echo -n " nfblockd"
-	start-stop-daemon --stop --quiet --pidfile $PIDFILE --signal HUP --exec $DAEMON
-	echo "."
-	;;
+        echo -n "Reloading $DESC:"
+        echo -n " nfblockd"
+        start-stop-daemon --stop --quiet --pidfile $PIDFILE --signal HUP --exec $DAEMON
+        echo "."
+        ;;
     restart)
-	$0 stop
-	$0 start
-	;;
+        $0 stop
+        $0 start
+        ;;
     *)
-	echo "Usage: /etc/init.d/nfblock {start|stop|reload|restart|force-reload}"
-	exit 1
+        echo "Usage: /etc/init.d/nfblock {start|stop|reload|restart|force-reload}"
+        exit 1
 esac
 
 exit 0
